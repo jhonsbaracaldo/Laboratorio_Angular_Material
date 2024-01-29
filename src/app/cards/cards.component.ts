9dfaf35fe8601d5ed733ef900c47228ff0c6dad6
jhonsbaracaldo <jhonsbaracaldo@gmail.com>
2024-01-29 14:25:04 -0500

    Laboratorio: Angular Material

---
+++ src/app/cards/cards.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+export class CardsComponent {
+  cards = [
+    {
+      title: 'Card 1',
+      img: 'https://picsum.photos/200/300',
+      desc: 'Description 1'
+    },
+    {
+      title: 'Card 2',
+      img: 'https://picsum.photos/200/300',
+      desc: 'Description 2'
+    },
+    {
+      title: 'Card 3',
+      img: 'https://picsum.photos/200/300',
+      desc: 'Description 3'
+    },
+    {
+      title: 'Card 4',
+      img: 'https://picsum.photos/200/300',
+      desc: 'Description 4'
+    }
+  ]
+
+}
